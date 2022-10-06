510627151b1c8f11d7fea7e1b9d2c61ad45f6cf1
AnnisaGhaidaRamadhani14 <113671744+AnnisaGhaidaRamadhani14@users.noreply.github.com>
2022-10-06 19:43:42 +0700

    Add files via upload

---
+++ untirta/fkip/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import Dosen, TenagaPendidik, Mahasiswa
+# Register your models here.
+
+admin.site.register(Dosen)
+admin.site.register(TenagaPendidik)
+admin.site.register(Mahasiswa)
