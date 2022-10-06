ecf34c1b2b3d22b2668f1951c6c805a24bc9deea
AnnisaGhaidaRamadhani14 <113671744+AnnisaGhaidaRamadhani14@users.noreply.github.com>
2022-10-06 19:45:03 +0700

    Delete views.py

--- untirta/fkip/views.py
+++
@@ -1,9 +0,0 @@
-import re
-from django.shortcuts import render
-
-# Create your views here.
-def prodi5(request):
-    return render(request, 'fkip/index.html')
-
-def akreditas5(request):
-    return render(request, 'fkip/akreditas.html')
